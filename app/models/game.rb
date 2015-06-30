9db55d2439c3b351abc10bfbf2f3254f0a4a88fb
Alan Covarrubias <aecovarru@gmail.com>
2015-06-30 16:30:27 -0700

    fix

--- app/models/game.rb
+++ app/models/game.rb
@@ -2,6 +2,6 @@ class Game < ActiveRecord::Base
 	belongs_to :away_team, :class_name => 'Team'
 	belongs_to :home_team, :class_name => 'Team'
 	def url
-		return self.home_team.abbr + self.year + self.month + self.day + self.num
+		return self.home_team.game_abbr + self.year + self.month + self.day + self.num
 	end
 end
--- db/migrate/20150630232010_add_team_abbr.rb
+++ db/migrate/20150630232010_add_team_abbr.rb
@@ -1,6 +1,6 @@
 class AddTeamAbbr < ActiveRecord::Migration
   def change
-  	add_column("teams", :team_abbr, :integer)
+  	add_column("teams", :team_abbr, :string)
   	add_index("teams", :team_abbr)
   end
 end
