3843759c1fa60045852487be4188e705fe168da6
Alan Covarrubias <aecovarru@gmail.com>
2015-07-17 13:18:02 -0700

    fux

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -50,7 +50,7 @@ class ApplicationController < ActionController::Base
 			games = Game.where("(home_team_id = #{home_team.id} OR away_team_id = #{home_team.id}) AND id < #{today_game.id}").order("id DESC")
 		else
 			if home_pitcher == nil
-				return [nil, Array.new]
+				return Array.new
 			end
 			throwhand = home_pitcher.throwhand
 			pitcher = away_pitcher
@@ -80,7 +80,7 @@ class ApplicationController < ActionController::Base
 					array = array[0...-1]
 					array << hitter
 				end
-				return [game, array]
+				return array
 			end
 		end
 	end
--- app/controllers/game_controller.rb
+++ app/controllers/game_controller.rb
@@ -101,13 +101,13 @@ class GameController < ApplicationController
 		if today_bool || @tomorrow_bool
 
 			if @away_hitters.size == 0
-				@projected_away_game, @away_hitters = findProjectedLineup(@game, false, @away_pitchers, @home_pitchers)
+				@away_hitters = findProjectedLineup(@game, false, @away_pitchers, @home_pitchers)
 				@away_hitters = getCurrentStats(@away_hitters)
 				@away_projected = true
 			end
 
 			if @home_hitters.size == 0
-				@projected_home_game, @home_hitters = findProjectedLineup(@game, true, @away_pitchers, @home_pitchers)
+				@home_hitters = findProjectedLineup(@game, true, @away_pitchers, @home_pitchers)
 				@home_hitters = getCurrentStats(@home_hitters)
 				@home_projected = true
 			end
