ea7fe382f26803150dbb89eada53b5c95a6670f4
Alan Covarrubias <aecovarru@gmail.com>
2016-04-06 17:43:22 -0700

    fix

--- app/models/game.rb
+++ app/models/game.rb
@@ -3,8 +3,6 @@ require 'open-uri'
 require 'timeout'
 
 class Game < ActiveRecord::Base
-
-  include Weather
   
   belongs_to :away_team, :class_name => 'Team'
   belongs_to :home_team, :class_name => 'Team'
