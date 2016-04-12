5034a315b613cbb8441e4ce4e97fe4560cb5796f
Alan Covarrubias <aecovarru@gmail.com>
2016-04-12 11:41:17 -0700

    fix download

--- lib/update.rb
+++ lib/update.rb
@@ -1,5 +1,7 @@
 module Update
 
+  include Share
+
   require 'nokogiri'
   require 'open-uri'
   require 'timeout'
@@ -11,14 +13,7 @@ module Update
   	  end
   	  url = "http://www.baseball-reference.com/players/split.cgi?id=#{pitcher.alias}&year=#{year}&t=p"
   	  puts url
-	    doc = nil
-	    begin
-	      Timeout::timeout(3){
-	  	    doc = Nokogiri::HTML(open(url))
-	  	  }
-	    rescue Errno::ECONNREFUSED, Timeout::Error, URI::InvalidURIError => e
-	  	  next
-	    end
+      doc = download_document(url)
   	  row = 0
   	  doc.css("#plato td").each_with_index do |element, index|
   	  	case index%28
