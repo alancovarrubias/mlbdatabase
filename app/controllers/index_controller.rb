60d99298b3a72c42c0d3cb2c91c93140bcc869e9
Alan Covarrubias <aecovarru@gmail.com>
2016-04-16 21:14:12 -0700

    fix

--- app/controllers/index_controller.rb
+++ app/controllers/index_controller.rb
@@ -1,4 +1,5 @@
 class IndexController < ApplicationController
+  require 'date'
 
   before_action :confirm_logged_in
 
@@ -25,11 +26,11 @@ class IndexController < ApplicationController
 		year = Time.now.year
 		bool = true
 		while bool
-			games = Game.where(:year => year.to_s)
+			games = GameDay.where(year: year)
 			if games.empty?
 				bool = false
 			else
-				@years << year
+				@years << year.to_s
 				year = year - 1
 			end
 		end
@@ -37,16 +38,11 @@ class IndexController < ApplicationController
 	end
 
 	def month
-		require 'date'
 		@months = Array.new
-		games = Game.where(:year => params[:year])
+		game_days = GameDay.where(year: params[:year])
 
-		(1..12).each do |i|
-			month = i.to_s
-			if i < 10
-				month = "0" + month
-			end
-			if !games.where(:month => month).empty?
+		(1..12).each do |month|
+			unless game_days.where(month: month).empty?
 				@months << month
 			end
 		end
@@ -56,26 +52,16 @@ class IndexController < ApplicationController
 	def day
 		require 'date'
 		@days = Array.new
-		games = Game.where(:year => params[:year], :month => params[:month])
+		game_days = GameDay.where(year: params[:year], month: params[:month])
 
-		(1..31).each do |i|
-			day = i.to_s
-			if i < 10
-				day = "0" + day
-			end
-			if !games.where(:day => day).empty?
+		(1..31).each do |day|
+			unless games.where(day: day).empty?
 				@days << day
 			end
 		end
 
-		@days = @days.map{|d| 
-			if d[0] == '0'
-				d_show = d[-1]
-			else
-				d_show = d
-			end
-
-			[d_show, d]
+		@days = @days.map{ |d|
+			[d, d]
 		}
 	end
 
