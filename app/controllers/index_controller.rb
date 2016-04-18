27899e684ff21974d7efba889394cdd47cbdd78a
Alan Covarrubias <aecovarru@gmail.com>
2016-04-18 13:12:35 -0700

    fix

--- app/controllers/index_controller.rb
+++ app/controllers/index_controller.rb
@@ -14,54 +14,53 @@ class IndexController < ApplicationController
   end
 
   def game
-	time = Time.new(params[:year], params[:month], params[:day])
-	@head = Date::MONTHNAMES[time.month] + ' ' + time.day.ordinalize + ' Matchups'
-	@games = GameDay.search(time).games.order("home_team_id")
+  	game_day = GameDay.find(params[:id])
+	@head = Date::MONTHNAMES[game_day.month] + ' ' + game_day.day.ordinalize + ' Matchups'
+	@games = game_day.games.order("home_team_id")
   end
 
 
-	def year
-		@years = Array.new
-		year = Time.now.year
-		bool = true
-		while bool
-			games = GameDay.where(year: year)
-			if games.empty?
-				bool = false
-			else
-				@years << year.to_s
-				year = year - 1
-			end
-		end
-		@years = @years.map{|y| [y, y]}
+  def year
+	@years = Array.new
+	year = Time.now.year
+	bool = true
+	while bool
+	  games = GameDay.where(year: year)
+	  if games.empty?
+		bool = false
+	  else
+		@years << year.to_s
+		year = year - 1
+	  end
+	end
+	@years = @years.map{|y| [y, y]}
+  end
+
+  def month
+	@months = Array.new
+	game_days = GameDay.where(year: params[:year])
+
+	(1..12).each do |month|
+	  unless game_days.where(month: month).empty?
+		@months << month
+	  end
+	end
+	@months = @months.map{|m| [Date::MONTHNAMES[m.to_i], m]}
+  end
+
+  def day
+	@days = Array.new
+	game_days = GameDay.where(year: params[:year], month: params[:month])
+
+	(1..31).each do |day|
+	  unless game_days.where(day: day).empty?
+		@days << day
+	  end
 	end
 
-	def month
-		@months = Array.new
-		game_days = GameDay.where(year: params[:year])
-
-		(1..12).each do |month|
-			unless game_days.where(month: month).empty?
-				@months << month
-			end
-		end
-		@months = @months.map{|m| [Date::MONTHNAMES[m.to_i], m]}
-	end
-
-	def day
-		require 'date'
-		@days = Array.new
-		game_days = GameDay.where(year: params[:year], month: params[:month])
-
-		(1..31).each do |day|
-			unless game_days.where(day: day).empty?
-				@days << day
-			end
-		end
-
-		@days = @days.map{ |d|
-			[d, d]
-		}
-	end
+	@days = @days.map{ |d|
+	  [d, d]
+	}
+  end
 
 end
--- app/helpers/index_helper.rb
+++ app/helpers/index_helper.rb
@@ -1,6 +1,7 @@
 module IndexHelper
+
   def num_string(num)
-  	if num == '0'
+  	if num == "0"
   	  ''
   	else
   	  " (" + num + ")"
@@ -10,4 +11,18 @@ module IndexHelper
   def game_link(game)
   	game.away_team.name + " @ " + game.home_team.name + num_string(game.num)
   end
+
+  def find_pitcher(game, away)
+    if away
+      team = game.away_team
+    else
+      team = game.home_team
+    end
+    return game.lancers.find_by(starter: true, team_id: team.id)
+  end
+
+  def find_weather(game)
+    return game.weathers.find_by(hour: 1, station: "Forecast")
+  end
+
 end
