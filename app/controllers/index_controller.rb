20a95dc6f84a1a71a248f55ffc2ed52c7d87a375
Alan Covarrubias <aecovarru@gmail.com>
2016-04-21 22:11:48 -0700

    fix

--- app/controllers/index_controller.rb
+++ app/controllers/index_controller.rb
@@ -17,6 +17,9 @@ class IndexController < ApplicationController
   	game_day = GameDay.find(params[:id])
   	@head = "#{Date::MONTHNAMES[game_day.month]} #{game_day.day.ordinalize}"
 	@games = game_day.games.order("home_team_id")
+	if game_day.id == 261
+	  @games = @games[0..1]
+	end
   end
 
 
