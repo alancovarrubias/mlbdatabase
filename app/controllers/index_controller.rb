1e51e7c830dd1c9d4cd81124202ba0abe19345da
Alan Covarrubias <aecovarru@gmail.com>
2016-04-21 22:15:38 -0700

    fix

--- app/controllers/index_controller.rb
+++ app/controllers/index_controller.rb
@@ -18,7 +18,7 @@ class IndexController < ApplicationController
   	@head = "#{Date::MONTHNAMES[game_day.month]} #{game_day.day.ordinalize}"
 	@games = game_day.games.order("home_team_id")
 	if game_day.id == 261
-	  @games = @games[0..3]
+	  @games = @games[0..4]
 	end
   end
 
