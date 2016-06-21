67bf81eb5d09dec6582a3fb44d860f9b07b2b801
Alan Covarrubias <aecovarru@gmail.com>
2016-06-21 15:33:58 -0700

    fix

--- app/models/game.rb
+++ app/models/game.rb
@@ -13,7 +13,9 @@ class Game < ActiveRecord::Base
   end
 
   def update_weather
-    Update::Weathers.new.update(self)
+    if game.weathers.where(station: "Actual").size == 0
+      Update::Weathers.new.update(self)
+    end
   end
 
   def update_forecast
--- db/migrate/20160620214533_add_air_density.rb
+++ db/migrate/20160620214533_add_air_density.rb
@@ -1,7 +1,7 @@
 class AddAirDensity < ActiveRecord::Migration
   def change
     add_column :weathers, :air_density, :float, default: 0.0
-    add_column :games, :temp, :float, default: 0.0
+    add_column :games, :temps, :float, default: 0.0
     add_column :games, :dew, :float, default: 0.0
     add_column :games, :baro, :float, default: 0.0
     add_column :games, :humid, :float, default: 0.0
